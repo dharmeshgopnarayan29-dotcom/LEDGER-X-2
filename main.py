558b7115c9ee03c7f0de932dc6bae755c427a479
Dharmesh <dharmeshgopnarayan29@gmail.com>
2025-12-06 02:57:16 +0530

    Change 4

--- main.py
+++ main.py
@@ -17,14 +17,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://127.0.0.1:5501",
-        "http://127.0.0.1:5502",
-        "http://localhost:5500",
-        "http://localhost:5501",
-        "http://localhost:5502"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
